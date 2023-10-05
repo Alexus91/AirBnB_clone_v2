be1e75aa9811671c13a0d1a9c00042ac84ade6d6
Alexus91 <mrfadlkamal@gmail.com>
2023-10-05 16:07:10 -0700

    0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,40 +1,24 @@
 #!/usr/bin/env bash
 
-# update and install Nginx if not installed already
+# setup web static
 sudo apt-get -y update
 sudo apt-get -y install nginx
 
-# necessary directories if they don't exist
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 
-# create a HTML file
-echo -e '<html>\n  <head>\n  </head>\n  <body>\n    Holberton School\n  </body>\n</html>' | sudo tee /data/web_static/releases/test/index.html > /dev/null
-
-# create or recreate symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# ubuntu user and group recursively
-sudo chown -R ubuntu:ubuntu /data/
-
-# configure Nginx to serve the content
-config_file="/etc/nginx/sites-available/default"
-config_content="server {
-    listen 80 default_server;
-    server_name _;
-
-    location /hbnb_static {
-        alias /data/web_static/current/;
-    }
-
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-}"
-
-echo "$config_content" | sudo tee "$config_file" > /dev/null
-
-sudo service nginx restart
-
+echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+config='\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;}'
+sed -i "38i $config" /etc/nginx/sites-available/default
+service nginx restart
 exit 0
 
