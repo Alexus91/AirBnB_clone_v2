bf1fce99229415dca1053f20305ded5367f5288e
Alexus91 <mrfadlkamal@gmail.com>
2023-10-05 17:26:04 -0700

    1. Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""Fabric script that generates a .tgz archive"""
-
+# Fabfile to generates a .tgz archive from web_static.
+from datetime import datetime
+import os.path
 from fabric.api import local
-import time
 
 
 def do_pack():
-    now = time.strftime("%Y%m%d%H%M%S")
-    try:
-        local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{}.tgz web_static/".
-              format(now))
-        return ("versions/web_static_{}.tgz".format(now))
-    except Exception as e:
+    """Create a .tgz file from web_static."""
+    date_t = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(date_t.year,
+                                                         date_t.month,
+                                                         date_t.day,
+                                                         date_t.hour,
+                                                         date_t.minute,
+                                                         date_t.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
+    return file
